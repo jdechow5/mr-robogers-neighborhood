a6f31c84e67b40c4ef47399d84a2a23e7db2ea64
Justin Dechow <justin.dechow@gmail.com>
2022-01-21 11:20:44 -0800

    WIP: updating beepBoopify(), currently inputting Beep but not the rest of the array

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@
 function beepBoopify(num) {
   let numberArray = [];
   for (let i = 0; i <= num; i++) {
-    numberArray.push(i);
+    if (numberArray.includes(1)) {
+      return "Beep!"
+    } else {
+      numberArray.push(i);
+    }
   }
   return numberArray;
 }
+
